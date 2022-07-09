a75a81440c48d874514c8c30077d30f17c257812
Dragoon8192 <dragoon8192@gmail.com>
2022-07-09 17:00:01 +0900

    update:   src/MyCanvas.purs
    Make a canvas.

--- src/MyCanvas.purs
+++ src/MyCanvas.purs
@@ -1,30 +1,79 @@
 module MyCanvas (component) where
 
 import Prelude
+import Data.Maybe (Maybe(..))
 import CSS
+import CSS.Common (auto)
+import Effect (Effect)
+import Effect.Aff.Class (class MonadAff)
+import Graphics.Canvas (CanvasElement)
 import Graphics.Canvas as Canvas
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.CSS (style)
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 
-data Action = Increment | Decrement
+data Action = Initialize
 
+type State = Int
+
+canvasId :: String
+canvasId = "myCanvas"
+
+canvasBackgroundColor :: Color
+canvasBackgroundColor = rgb 238 238 238
+
+component :: forall query input output m. MonadAff m => H.Component query input output m
 component =
-  H.mkComponent {
-    initialState, render
-    , eval: H.mkEval $ H.defaultEval
-      { handleAction = handleAction }
+  H.mkComponent
+    { initialState
+    , render
+    , eval
     }
-  where
-  initialState _ = 0
 
-  render state =
-    HH.div_
-      [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
-      , HH.div_ [ HH.text $ show state ]
-      , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
+initialState :: forall i. i -> State
+initialState _ = 0
+
+render :: forall m. State -> H.ComponentHTML Action () m
+render _ =
+    HH.main_
+      [ HH.canvas
+          [ style do
+              background canvasBackgroundColor
+              display block
+              marginTop (px 0.0)
+              marginRight auto
+              marginLeft auto
+              marginBottom (px 0.0)
+          , HP.id canvasId
+          , HP.width 480
+          , HP.height 480
+          ]
       ]
 
-  handleAction = case _ of
-    Increment -> H.modify_ \state -> state + 1
-    Decrement -> H.modify_ \state -> state - 1
+eval = H.mkEval $ H.defaultEval
+        { handleAction = handleAction
+        , initialize = Just Initialize
+        }
+
+handleAction Initialize = do
+  maybeCanvas <- H.liftEffect $ Canvas.getCanvasElementById canvasId
+  H.liftEffect $ case maybeCanvas of
+                     Nothing -> pure unit
+                     Just canvas -> draw canvas
+
+draw :: CanvasElement -> Effect Unit
+draw canvas = do
+  ctx <- Canvas.getContext2D canvas
+  --
+  Canvas.beginPath ctx
+  Canvas.rect ctx $
+    { x: 240.0
+    , y: 160.0
+    , width: 100.0
+    , height: 40.0
+    }
+  Canvas.setFillStyle ctx "blue"
+  Canvas.fill ctx
+  Canvas.closePath ctx
