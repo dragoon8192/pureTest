d775c58da3ced7197c041964d0f315675633c649
Dragoon8192 <dragoon8192@gmail.com>
2022-07-10 11:57:14 +0900

    update:   src/MyCanvas.purs
    Move a circle.

--- src/MyCanvas.purs
+++ src/MyCanvas.purs
@@ -1,10 +1,14 @@
 module MyCanvas (component) where
 
 import Prelude
-import Data.Maybe (Maybe(..))
-import CSS
+import Control.Monad.Rec.Class (forever)
+import Data.Maybe (Maybe(..), maybe)
+import Data.Tuple
 import CSS.Common (auto)
+import CSS
 import Effect (Effect)
+import Effect.Aff (Milliseconds(..))
+import Effect.Aff as Aff
 import Effect.Aff.Class (class MonadAff)
 import Graphics.Canvas (CanvasElement)
 import Graphics.Canvas as Canvas
@@ -13,10 +17,17 @@ import Halogen.HTML as HH
 import Halogen.HTML.CSS (style)
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
+import Halogen.Subscription as HS
+import Data.Number (pi)
 
-data Action = Initialize
+data Action = Initialize | Tick
 
-type State = Int
+type State =  { mbCanvas :: Maybe CanvasElement
+              , dataset :: Dataset
+              }
+type Dataset = { q :: Tuple Number Number
+               , dq :: Tuple Number Number
+               }
 
 canvasId :: String
 canvasId = "myCanvas"
@@ -33,7 +44,12 @@ component =
     }
 
 initialState :: forall i. i -> State
-initialState _ = 0
+initialState _ =  { mbCanvas : Nothing
+                  , dataset :
+                    { q : Tuple 480.0 480.0
+                    , dq : Tuple 1.0 1.0
+                    }
+                  }
 
 render :: forall m. State -> H.ComponentHTML Action () m
 render _ =
@@ -47,8 +63,8 @@ render _ =
               marginLeft auto
               marginBottom (px 0.0)
           , HP.id canvasId
-          , HP.width 480
-          , HP.height 480
+          , HP.width 960
+          , HP.height 960
           ]
       ]
 
@@ -58,21 +74,48 @@ eval = H.mkEval $ H.defaultEval
         }
 
 handleAction Initialize = do
-  maybeCanvas <- H.liftEffect $ Canvas.getCanvasElementById canvasId
-  H.liftEffect $ case maybeCanvas of
-                     Nothing -> pure unit
-                     Just canvas -> draw canvas
+  mbCanvas <- H.liftEffect $ Canvas.getCanvasElementById canvasId
+  H.modify_ \state -> state { mbCanvas = mbCanvas }
+  _ <- H.subscribe =<< timer Tick
+  mempty
+handleAction Tick = do
+  tickUpdateDataset
+  state <- H.get
+  H.liftEffect $ draw state.mbCanvas state.dataset
+  where
+    tickUpdateDataset = do
+      state <- H.get
+      let { q , dq } = state.dataset
+      H.modify_ \s -> s { dataset = { q : (q + dq), dq : dq }}
 
-draw :: CanvasElement -> Effect Unit
-draw canvas = do
+timer :: forall m a. MonadAff m => a -> m (HS.Emitter a)
+timer val = do
+  { emitter, listener } <- H.liftEffect HS.create
+  _ <- H.liftAff $ Aff.forkAff $ forever do
+    Aff.delay $ Milliseconds 10.0
+    H.liftEffect $ HS.notify listener val
+  pure emitter
+
+draw :: Maybe CanvasElement -> Dataset -> Effect Unit
+draw Nothing _ = mempty
+draw (Just canvas) ds = do
   ctx <- Canvas.getContext2D canvas
+  dim <- Canvas.getCanvasDimensions canvas
   --
+  Canvas.clearRect ctx $
+    { x: 0.0
+    , y: 0.0
+    , width: dim.width
+    , height: dim.height
+    }
   Canvas.beginPath ctx
-  Canvas.rect ctx $
-    { x: 240.0
-    , y: 160.0
-    , width: 100.0
-    , height: 40.0
+  Canvas.arc ctx $
+    { x: fst ds.q
+    , y: snd ds.q
+    , radius: 30.0
+    , start: 0.0
+    , end: pi * 2.0
+    , useCounterClockwise: true
     }
   Canvas.setFillStyle ctx "blue"
   Canvas.fill ctx
