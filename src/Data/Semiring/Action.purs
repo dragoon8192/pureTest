7b757b9b4b3a34ce7feb379f23526fd93f52bbe1
Dragoon8192 <dragoon8192@gmail.com>
2022-07-18 12:25:34 +0900

    update:   src/Data/Semiring/Action.purs

--- src/Data/Semiring/Action.purs
+++ src/Data/Semiring/Action.purs
@@ -1,15 +1,22 @@
 module Data.Semiring.Action where
 
-import Prelude (class Semiring)
-import Data.Group.Action as GA
+import Data.Semiring (class Semiring)
+import Data.Monoid (class Monoid)
+import Data.Semigroup.Commutative (class Commutative)
 
--- | A semiring `a` acting on a semimodule `x`. Instances must satisfy the following
+-- | A semiring `a` acting on a semimodule (a commutative monoid`x`. Instances must satisfy the following
 -- | laws in addition to the `Semiring` laws:
 -- |
--- | - Identity: `forall v. lact one v = v`
--- |   - i.e. `lact one = id`
--- | - Compatibility: `forall k l v. lact (k * l) v = lact k (lact l v)`
--- |   - i.e. `forall k l. lact (k * l) = lact k <<< lact l
-class (Semiring k) <= LeftAction k v where
-  lact :: k -> v -> v
+-- | - Identity: `forall x. lact one x = x`
+-- |   - i.e. `lact one = identity`
+-- | - Compatibility: `forall a b x. lact (a * b) x = lact a (lact b x)`
+-- |   - i.e. `forall a b. lact (a * b) = lact a <<< lact b
+-- | - Identity: `forall x. lact zero x = mempty`
+-- |   - i.e. `lact zero = mempty`
+-- | - Distributivity: `forall a b x. lact (a + b) x = lact a x <> lact b x`
+-- |   - i.e. `forall a b. lact (a + b) = lact a <> lact b`
+-- | - Distributivity: `forall a x y. lact a (x <> y) = lact a x <> lact a y`
+-- |   - i.e. `forall a. lact a` is monoid endomorphism.
+class (Semiring a, Commutative x, Monoid x) <= LeftAction a x where
+  lact :: a -> x -> x
 
