c28f725132d1e74cc08eb815ed9a60a6eac1f324
Dragoon8192 <dragoon8192@gmail.com>
2022-07-18 11:50:22 +0900

    fix:   src/Data/Field/Action.purs new:   src/Data/Semiring/Action.purs

--- src/Data/Semiring/Action.purs
+++ src/Data/Semiring/Action.purs
@@ -1,16 +1,15 @@
-module Data.Field.Action where
+module Data.Semiring.Action where
 
-import Prelude (class Field)
+import Prelude (class Semiring)
 import Data.Group.Action as GA
 
--- #WIP
--- | A field `k` acting on a vector-space `v`. Instances must satisfy the following
--- | laws in addition to the `` laws:
+-- | A semiring `a` acting on a semimodule `x`. Instances must satisfy the following
+-- | laws in addition to the `Semiring` laws:
 -- |
 -- | - Identity: `forall v. lact one v = v`
 -- |   - i.e. `lact one = id`
 -- | - Compatibility: `forall k l v. lact (k * l) v = lact k (lact l v)`
 -- |   - i.e. `forall k l. lact (k * l) = lact k <<< lact l
-class (Field k) <= LeftAction k v where
+class (Semiring k) <= LeftAction k v where
   lact :: k -> v -> v
 
