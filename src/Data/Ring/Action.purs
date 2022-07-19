2286761891fe8090506a2f6c0a4ed311a5d237e6
Dragoon8192 <dragoon8192@gmail.com>
2022-07-19 19:03:43 +0900

    update:   src/Data/Ring/Action.purs
    Re-exports from Data.Semiring.Action

--- src/Data/Ring/Action.purs
+++ src/Data/Ring/Action.purs
@@ -1,9 +1,21 @@
 module Data.Ring.Action
   ( class LeftAction
   , class RightAction
+  , module Data.Semiring.Action
   ) where
 
-import Data.Semiring.Action (class LeftAction, class RightAction) as SemiringAct
+import Data.Semiring.Action
+  ( class LeftAction
+  , class RightAction
+  ) as SemiringAct
+import Data.Semiring.Action
+  ( lact
+  , ract
+  , flipRact
+  , (<+)
+  , (+>)
+  )
+
 import Data.Ring (class Ring)
 
 class (SemiringAct.LeftAction a x, Ring a) <= LeftAction a x
