229f95436cb801ed538a78184fc0b0d706e9ff9f
Dragoon8192 <dragoon8192@gmail.com>
2022-07-19 19:47:19 +0900

    update:   src/Data/Semiring/Action.purs
    Delete a 'Commutative' condition. Add instances of semiring acting on
    itself.

--- src/Data/Semiring/Action.purs
+++ src/Data/Semiring/Action.purs
@@ -9,9 +9,9 @@ module Data.Semiring.Action
   ) where
 
 import Data.Function (flip)
-import Data.Semiring (class Semiring)
+import Data.Semiring (class Semiring, (*))
 import Data.Monoid (class Monoid)
-import Data.Semigroup.Commutative (class Commutative)
+import Data.Monoid.Multiplicative
 
 -- | A semiring `a` acting on a semimodule (a commutative monoid`x`. Instances must satisfy the following
 -- | laws in addition to the `Semiring` laws:
@@ -26,7 +26,7 @@ import Data.Semigroup.Commutative (class Commutative)
 -- |   - i.e. `forall a b. lact (a + b) = lact a <> lact b`
 -- | - Distributivity: `forall a x y. lact a (x <> y) = lact a x <> lact a y`
 -- |   - i.e. `forall a. lact a` is monoid endomorphism.
-class (Semiring a, Commutative x, Monoid x) <= LeftAction a x where
+class (Semiring a, Monoid x) <= LeftAction a x where
   lact :: a -> x -> x
 
 -- | A semiring `a` acting on a semimodule (a commutative monoid`x`. Instances must satisfy the following
@@ -42,7 +42,7 @@ class (Semiring a, Commutative x, Monoid x) <= LeftAction a x where
 -- |   - i.e. `forall a b. ract (a + b) = ract a <> ract b`
 -- | - Distributivity: `forall a x y. ract a (x <> y) = ract a x <> ract a y`
 -- |   - i.e. `forall a. ract a` is monoid endomorphism.
-class (Semiring a, Commutative x, Monoid x) <= RightAction a x where
+class (Semiring a, Monoid x) <= RightAction a x where
   ract :: a -> x -> x
 
 flipRact :: forall a x. RightAction a x => x -> a -> x
@@ -50,3 +50,9 @@ flipRact = flip ract
 
 infixl 6 lact as <+
 infixr 6 flipRact as +>
+
+instance leftActionSemiringActingOnItself :: Semiring a => LeftAction a (Multiplicative a) where
+  lact a (Multiplicative b) = Multiplicative (a * b)
+
+instance rightActionSemiringActingOnItself :: Semiring a => RightAction a (Multiplicative a) where
+  ract a (Multiplicative b) = Multiplicative (b * a)
