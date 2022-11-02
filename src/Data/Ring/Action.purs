29aa2c6626bbaa8af4656272e874c6a4b212a33e
Dragoon8192 <dragoon8192@gmail.com>
2022-11-02 18:45:38 +0900

    update:   src/Data/Ring/Action.purs
    Remove sentences about Group.

--- src/Data/Ring/Action.purs
+++ src/Data/Ring/Action.purs
@@ -5,7 +5,6 @@ module Data.Ring.Action
   ) where
 
 import Data.Ring (class Ring)
-
 import Data.Semiring.Action
   ( class LeftAction
   , class RightAction
@@ -18,9 +17,24 @@ import Data.Semiring.Action
   , (^>)
   )
 
+
+-- | A ring `a` acting on a module (a monoid`x`).
+-- | Instances automatically satisfies the following laws in addition to the `Semiring Action` laws:
+-- |
+-- | - Inverse: `forall a b x. lact (- a) x = ginverse $ lact a x
 class (Semiring.LeftAction a x, Ring a) <= LeftAction a x
 
+-- | A ring `a` acting on a module (a monoid`x`).
+-- | Instances automatically satisfies the following laws in addition to the `Semiring Action` laws:
+-- |
+-- | - Inverse: `forall a b x. ract (- a) x = - ract a x
 class (Semiring.RightAction a x, Ring a) <= RightAction a x
 
 instance leftActionAuto :: (Semiring.LeftAction a x, Ring a) => LeftAction a x
 instance rightActionAuto :: (Semiring.RightAction a x, Ring a) => RightAction a x
+
+-- instance groupThatRingLeftActOn :: (LeftAction a x) => Group x where
+--   ginverse = lact (negate one)
+-- instance groupThatRingRightActOn :: (RightAction a x) => Group x where
+--   ginverse = ract (negate one)
+
