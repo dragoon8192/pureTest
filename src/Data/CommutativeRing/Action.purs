265f6f6d3395cfb9aa234a88ae430025a9bd4435
Dragoon8192 <dragoon8192@gmail.com>
2022-11-02 18:45:25 +0900

    fix:   src/Data/CommutativeRing/Action.purs

--- src/Data/CommutativeRing/Action.purs
+++ src/Data/CommutativeRing/Action.purs
@@ -2,11 +2,6 @@ module Data.CommutativeRing.Action
   ( class LeftAction
   , class RightAction
   , module Data.Ring.Action
-  , lact
-  , ract
-  , flipRact
-  , (<^)
-  , (^>)
   ) where
 
 import Data.CommutativeRing (class CommutativeRing)
@@ -32,8 +27,8 @@ class (Ring.LeftAction a x, CommutativeRing a) <= LeftAction a x
 -- | A commutative ring `a` acting on a module (a monoid`x`).
 -- | Instances automatically satisfies the following laws in addition to the `Ring Action` laws:
 -- |
--- | - Commutativity: `forall a b x. lact (a * b) x = lact (b * a) x
--- |   - i.e. `ract a >>> lact b = ract b >>> lact a`
+-- | - Commutativity: `forall a b x. ract (a * b) x = ract (b * a) x
+-- |   - i.e. `ract a >>> ract b = ract b >>> ract a`
 class (Ring.RightAction a x, CommutativeRing a) <= RightAction a x
 
 instance leftActionAuto :: (Ring.LeftAction a x, CommutativeRing a) => LeftAction a x
