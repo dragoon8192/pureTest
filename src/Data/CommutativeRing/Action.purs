d4a2c4053184b5eadd96a15f1bf09e51cc932bb6
Dragoon8192 <dragoon8192@gmail.com>
2022-11-02 19:13:32 +0900

    fix:   src/Data/CommutativeRing/Action.purs

--- src/Data/CommutativeRing/Action.purs
+++ src/Data/CommutativeRing/Action.purs
@@ -20,14 +20,14 @@ import Data.Ring.Action
 -- | A commutative ring `a` acting on a module (a group`x`).
 -- | Instances automatically satisfies the following laws in addition to the `Ring Action` laws:
 -- |
--- | - Commutativity: `forall a b x. lact (a * b) x = lact (b * a) x
+-- | - Commutativity: `forall a b x. lact (a * b) x = lact (b * a) x`
 -- |   - i.e. `lact a >>> lact b = lact b >>> lact a`
 class (Ring.LeftAction a x, CommutativeRing a) <= LeftAction a x
 
 -- | A commutative ring `a` acting on a module (a group`x`).
 -- | Instances automatically satisfies the following laws in addition to the `Ring Action` laws:
 -- |
--- | - Commutativity: `forall a b x. ract (a * b) x = ract (b * a) x
+-- | - Commutativity: `forall a b x. ract (a * b) x = ract (b * a) x`
 -- |   - i.e. `ract a >>> ract b = ract b >>> ract a`
 class (Ring.RightAction a x, CommutativeRing a) <= RightAction a x
 
